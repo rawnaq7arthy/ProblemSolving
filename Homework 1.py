c0a9359fc542d3503d87aa531659ea9be5538a16
rawnaq7arthy <151367242+rawnaq7arthy@users.noreply.github.com>
2025-05-22 21:33:53 +0400

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,41 @@
+#Problem 1:
+name = "Ali"
+age = 25
+height = 1.68
+print(f"Hello, my name is {name}. I am {age} years old and my height is{height} meters.")
+
+#Problem 2:
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+#Problem 3:
+a = 10
+b = 3.14
+c = "Hello, world!"
+print("The type of variable a is", type(a))
+print("The type of variable b is", type(b))
+print("The type of variable c is", type(c))
+
+#Problem 4:
+length = 10
+width = 5
+area = length * width
+print("Length:", length)
+print("Width:", width)
+print("The area of the rectangle is", area)
+
+
+#Problem 5:
+P = float(input("Enter Principal amount: "))
+R = float(input("Enter Rate of Interest: "))
+T = float(input("Enter Time (in years): "))
+simple_interest = (P * R * T) / 100
+print("The Simple Interest is", simple_interest)
